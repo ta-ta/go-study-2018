a5c65f5ff50af1456c049e737587ca42abca01d6
tata <tata@tata.local>
2019-03-11 01:57:31 +0900

    講義資料の内容を修正

--- syntax/for.go
+++ syntax/for.go
@@ -8,37 +8,41 @@ func main() {
 		// 10回実行される
 		fmt.Printf("i = %d\n", i)
 	}
-
+	fmt.Println()
 	fmt.Println()
 
-	// 条件文のみでも繰り返し可能
-	j := 1
-	for j < 20 {
-		fmt.Printf("j = %d\n", j)
-		j *= 2
-	}
-
-	fmt.Println()
-
-	// スライスの要素を列挙する
-	// i が index, e が要素として代入される
-	s := []int{1, 2, 3, 4, 5}
-	for i, e := range s {
-		fmt.Printf("i = %d, e = %d\n", i, e)
-	}
-
-	fmt.Println()
-
-	// 文字列の要素を列挙する
+	// 文字列を一文字ずつ表示する
 	str := "Hello World"
 	for i, c := range str {
 		fmt.Printf("i = %d, c = %c\n", i, c)
 	}
-
+	fmt.Println()
 	fmt.Println()
 
 	// インデックスを使わない場合 -> `_` に代入
 	for _, c := range str {
 		fmt.Printf("c = %c\n", c)
 	}
+	fmt.Println()
+	fmt.Println()
+
+	/*
+		// 条件文のみでも繰り返し可能
+		j := 1
+		for j < 20 {
+			fmt.Printf("j = %d\n", j)
+			j *= 2
+		}
+		fmt.Println()
+		fmt.Println()
+
+		// スライスの要素を列挙する
+		// i が index, e が要素として代入される
+		s := []int{1, 2, 3, 4, 5}
+		for i, e := range s {
+			fmt.Printf("i = %d, e = %d\n", i, e)
+		}
+		fmt.Println()
+		fmt.Println()
+	*/
 }
--- syntax/var.go
+++ syntax/var.go
@@ -3,65 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	// 変数の宣言
-	// var 変数名 型名 の形で宣言する
-	var v1 int
-
-	// 宣言された直後は0値と呼ばれる値に初期化される
-	fmt.Printf("v1 = %d\n\n", v1)
-
-	// 変数への値の代入
-	// 変数名 = 値
-	v1 = 1
-	fmt.Printf("v1 = %d\n", v1)
-	v1 = 2
-	fmt.Printf("v1 = %d\n\n", v1)
-
-	// 値の基本型と0値
-	var b1 bool
+	// 基本型と変数宣言
+	// var 変数名 型名
 	var i1 int
-	var s1 string
 	var f1 float32
-	fmt.Printf("b1 = %t, i1 = %d, s1 = '%s', f1 = %f\n\n", b1, i1, s1, f1)
+	var s1 string
+	var b1 bool
+
+	// 宣言された直後は0値に初期化される
+	fmt.Printf("宣言と初期化\n")
+	fmt.Printf("int型の変数 i1 = %d\n", i1)
+	fmt.Printf("float型の変数 f1 = %f\n", f1)
+	fmt.Printf("string型の変数 s1 = %s\n", s1)
+	fmt.Printf("bool型の変数 b1 = %t\n", b1)
+	fmt.Printf("\n\n")
+
+	// 変数へ値の代入
+	// 変数名 = 値
+	i1 = 10
+	fmt.Printf("代入\n")
+	fmt.Printf("i1 = %d\n", i1)
+	fmt.Printf("\n\n")
 
 	// 変数の宣言と代入を同時に行う
 	// "" で囲まれた値はstring(文字列)として解釈される
-	v2 := "v2"
-	fmt.Printf("v2 = '%s'\n\n", v2)
-
-	// 既存の変数を再宣言しようとするとエラー
-	// v1 := 3
-
-	// 宣言した型と違う値を代入してもエラー
-	// v1 = "v1"
-
-	// 定数はconstで宣言できる
-	const c1 = 1
-	fmt.Printf("c1 = %d\n\n", c1)
-
-	// 定数値は値を変更できない
-	// c1 = 2
-
-	// 変数はスコープごとで別に記録される
-	{
-		// ここではv1は外側のスコープの変数の値を参照する
-		fmt.Printf("v1 = %d\n", v1)
-		// スコープ内の変数を定義
-		v1 := 100
-		fmt.Printf("v1 = %d\n", v1)
-	}
-	// スコープ内での新変数への操作は影響しない
-	fmt.Printf("v1 = %d\n\n", v1)
+	s2 := "s2"
+	fmt.Printf("宣言と代入\n")
+	fmt.Printf("s2 = '%s'\n", s2)
+	fmt.Printf("\n\n")
 
 	// 応用構文
 	// 複数の同じ型の変数を同時に宣言
-	var v3, v4 int
+	var i2, i3 int
+	fmt.Printf("i2 = %d, i3 = %d\n", i2, i3)
 
-	// 同時複数の変数に代入
-	v3, v4 = 3, 4
-	fmt.Printf("v3 = %d, v4 = %d\n\n", v3, v4)
-
-	// 同時に複数の型の変数を宣言
+	// 複数の型の変数を宣言
 	var (
 		vg1 int
 		vg2 string
@@ -69,4 +45,33 @@ func main() {
 	)
 	vg1, vg2, vg3 = 1, "2", 3.0
 	fmt.Printf("vg1 = %d, vg2 = %s, vg3 = %f\n", vg1, vg2, vg3)
+
+	/*
+		// 既存の変数を再宣言しようとするとエラー
+		// i1 := 200
+
+		// 宣言した型と違う値を代入してもエラー
+		// i1 = "300"
+
+		// 定数はconstで宣言できる
+		const const_i1 = 1
+		fmt.Printf("定数\n")
+		fmt.Printf("const_i1 = %d\n", const_i1)
+		fmt.Printf("\n\n")
+		// 定数値は値を変更できない
+		// c1 = 2
+
+		// 変数はスコープごとに区別して記録される
+		fmt.Printf("スコープ\n")
+		{
+			// ここではi1は外側のスコープの変数の値を参照する
+			fmt.Printf("i1 = %d\n", i1)
+			// スコープ内の変数を定義
+			i1 := 100
+			fmt.Printf("i1 = %d\n", i1)
+		}
+		// スコープ内での新変数への操作は影響しない
+		fmt.Printf("i1 = %d\n", i1)
+		fmt.Printf("\n\n")
+	*/
 }
