e6f81a2c7e6cbd137cf93fe8748e1849a9a7f4f3
tata <tata@tata.local>
2019-03-13 01:08:57 +0900

    使用する関数を修正

--- file/file.go
+++ file/file.go
@@ -1,45 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 )
 
 func main() {
-	ReadFile("test.txt")
+	ReadFileLine("file/test.txt")
 
 	/*
-		ReadFileLine("test.txt")
+		ReadFile("file/test.txt")
 	*/
 }
 
-// ReadFile ファイルの内容を一括で読み込む
-func ReadFile(filename string) {
-	// ファイルを開く
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// ファイルの内容全てを読む
-	data, err := ioutil.ReadAll(file) // byte配列を返す
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// 内容を出力
-	fmt.Printf("===== %s\n", filename)
-	fmt.Printf("%v\n", data) // []byteも%s
-	fmt.Println("=====")
-
-	// ファイルを閉じる
-	file.Close()
-}
-
-/*
 // ReadFileLine ファイルの内容を一行ずつ読み込む
 func ReadFileLine(filename string) {
 	// ファイルを開く
@@ -60,6 +35,32 @@ func ReadFileLine(filename string) {
 		fmt.Printf("%d行目: %s\n", i, line)
 		i++
 	}
+
+	// ファイルを閉じる
+	file.Close()
+}
+
+/*
+// ReadFile ファイルの内容を一括で読み込む
+func ReadFile(filename string) {
+	// ファイルを開く
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// ファイルの内容全てを読む
+	data, err := ioutil.ReadAll(file) // byte配列を返す
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 内容を出力
+	fmt.Printf("===== %s\n", filename)
+	fmt.Printf("%s\n", data) // []byteも%s
+	fmt.Println("=====")
 
 	// ファイルを閉じる
 	file.Close()
