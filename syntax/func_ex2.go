63cdfeb5357291b3d244a59b8448f46d7c38a179
tata <tata@tata.local>
2019-03-13 01:21:37 +0900

    コメント文言修正

--- syntax/func_ex2.go
+++ syntax/func_ex2.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// 配列B を受け取って要素の 2倍にした配列を返す関数Double(B) を実装
+	// 配列B を受け取って要素の 2倍にした配列を返す関数Double(B) を作成する
 	B := []int{1, 9, 3, 5}
 	fmt.Printf("Double(B) = %v\n", Double(B))
 	// 出力例: "Double(B) = [2 18 6 10]"
